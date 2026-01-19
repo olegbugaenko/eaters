ace69edcda31bf97c583ee9636cef8c9d8a005ff
Oleh <oleh.buhaienko@chatbots.studio>
2026-01-19 17:22:29 +0200

    ajust-arc-params

--- src/db/arcs-db.ts
+++ src/db/arcs-db.ts
@@ -25,8 +25,8 @@ const FREEZE_ARC_COLOR: SceneColor = { r: 0.6, g: 0.85, b: 1.0, a: 0.9 };
 const FREEZE_ARC_BLUR: SceneColor = { r: 0.4, g: 0.7, b: 1.0, a: 0.5 };
 const LASER_ARC_COLOR: SceneColor = { r: 1.0, g: 0.65, b: 0.7, a: 0.99 };
 const LASER_ARC_BLUR: SceneColor = { r: 1.0, g: 0.65, b: 0.7, a: 0.25 };
-const CHAIN_ARC_COLOR: SceneColor = { r: 0.45, g: 0.8, b: 1.0, a: 0.95 };
-const CHAIN_ARC_BLUR: SceneColor = { r: 0.3, g: 0.7, b: 1.0, a: 0.45 };
+const CHAIN_ARC_COLOR: SceneColor = { r: 0.85, g: 0.95, b: 1.0, a: 0.95 };
+const CHAIN_ARC_BLUR: SceneColor = { r: 0.3, g: 0.7, b: 1.0, a: 0.35 };
 
 const ARC_DB: Record<ArcType, ArcConfig> = {
   heal: {
@@ -83,14 +83,14 @@ const ARC_DB: Record<ArcType, ArcConfig> = {
   chainLightning: {
     coreColor: CHAIN_ARC_COLOR,
     blurColor: CHAIN_ARC_BLUR,
-    coreWidth: 1.5,
-    blurWidth: 18,
+    coreWidth: 0.15,
+    blurWidth: 5,
     lifetimeMs: 700,
     fadeStartMs: 350,
     bendsPer100Px: 3,
-    noiseAmplitude: 6,
-    oscillationPeriodMs: 220,
-    oscillationAmplitude: 0.9,
+    noiseAmplitude: 16,
+    oscillationPeriodMs: 170,
+    oscillationAmplitude: 0.4,
   },
 };
 
